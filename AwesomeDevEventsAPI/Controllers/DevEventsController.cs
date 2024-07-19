32a4404439d540f81283d2bc05d3123d24e1cdae
andsilva515 <anderson.ssilva@outlook.com>
2024-07-19 12:09:01 -0300

    criacao controller e persistencia dados

---
+++ AwesomeDevEventsAPI/Controllers/DevEventsController.cs
@@ -0,0 +1,63 @@
+﻿using AwesomeDevEventsAPI.Persistence;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AwesomeDevEventsAPI.Controllers
+{
+    [Route("api/[dev-events]")]
+    [ApiController]
+    public class DevEventsController : ControllerBase
+    {
+        private readonly DevEventsDbContext _context;
+
+        public DevEventsController(DevEventsDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var devEvents = _context.DevEvents.Where(d => d.IsDeleted).ToList();
+            
+            return Ok(devEvents);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(Guid id)
+        {
+            var devEvent = _context.DevEvents.SingleOrDefault(d => d.Id == id);
+
+            if (devEvent == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(devEvent);
+
+        }
+
+
+        [HttpPost]
+        public IActionResult Post(DevEvent devEvent)
+        {
+
+        }      
+
+        //api-/dev-events/12312421 PUT
+        [HttpPut("{id}")]
+        public IActionResult Update(Guid id, DevEvent devEvent)
+        {
+
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(Guid id)
+        {
+
+        }
+        
+
+    }
+
+}
